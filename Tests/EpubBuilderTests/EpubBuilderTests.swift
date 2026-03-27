132af8f1062f173459000bf83288592178bf066f
Chen Liang <87241041+Chen2357@users.noreply.github.com>
2026-03-26 22:59:08 -0700

    Add `Book3` output

--- Tests/EpubBuilderTests/EpubBuilderTests.swift
+++ Tests/EpubBuilderTests/EpubBuilderTests.swift
@@ -36,7 +36,7 @@ var book1FileSystemContent: FileSystemNode {
         }
         Folder("style") { }
         Folder("text") {
-            File("p-0001.xhtml")
+            File("p-001.xhtml")
         }
     }
 }
@@ -63,37 +63,31 @@ func book2() throws -> Book {
         title: "タイトル",
         creators: [.init(name: "作者", role: .author), .init(name: "イラストレーター", role: .illustrator)],
         language: "ja",
-        sections: [
+        sections: [1, 2, 3, 4, 10, 11, 12, 100].map { index in
             BookSection(
-                title: "第１話",
+                title: "第<span class=\"tcy\">\(index)</span>話",
                 content: [
-                    .paragraph("第１話の内容です。")
-                ]),
-            BookSection(
-                title: "第２話",
-                content: [
-                    .paragraph("第２話の内容です。")
+                    .paragraph("第<span class=\"tcy\">\(index)</span>話の内容です。")
                 ])
-        ],
-        images: [.init(name: "cover.jpg", data: try getCoverImageData(), mediaType: .jpeg)],
-        coverImageName: "cover.jpg")
+        })
 }
 
 @Test("Test depth one navigation document generation")
 func testBook2Navigation() throws {
-    let builder = FancyVrtlEpubBuilder(
-        depth: .one,
-        sectionTitleFormatter: { number, title in
-            "#\(number[0]). \(title)"
-        }
-    )
+    let builder = FancyVrtlEpubBuilder(depth: .one)
 
     let expectedNavigation = """
     <nav epub:type="toc">
     <h1>Contents</h1>
     <ol>
-    <li><a href="text/p-0001.xhtml">#1. 第１話</a></li>
-    <li><a href="text/p-0002.xhtml">#2. 第２話</a></li>
+    <li><a href="p-001.xhtml">第<span class="tcy">1</span>話</a></li>
+    <li><a href="p-002.xhtml">第<span class="tcy">2</span>話</a></li>
+    <li><a href="p-003.xhtml">第<span class="tcy">3</span>話</a></li>
+    <li><a href="p-004.xhtml">第<span class="tcy">4</span>話</a></li>
+    <li><a href="p-005.xhtml">第<span class="tcy">10</span>話</a></li>
+    <li><a href="p-006.xhtml">第<span class="tcy">11</span>話</a></li>
+    <li><a href="p-007.xhtml">第<span class="tcy">12</span>話</a></li>
+    <li><a href="p-008.xhtml">第<span class="tcy">100</span>話</a></li>
     </ol>
     </nav>
     """
@@ -103,17 +97,18 @@ func testBook2Navigation() throws {
 
 @Test("Test depth one Epub generation")
 func testBook2Epub() throws {
-    let book = try book2().toEpub(builder: FancyVrtlEpubBuilder(
-        depth: .one,
-        sectionTitleFormatter: { number, title in
-            "#\(number[0]). \(title)"
-        }
-    ), bookId: .zero)
+    let book = try book2().toEpub(builder: FancyVrtlEpubBuilder(depth: .one), bookId: .zero)
     let outputURL = try getOutputFolderURL().appendingPathComponent("Book2")
     try FileManager.default.ensureDirectoryExists(at: outputURL)
     try book.write(to: outputURL)
 }
 
+@Test func outputBook2Epub() throws {
+    let book = try book2().toEpub(builder: FancyVrtlEpubBuilder(depth: .one), bookId: .zero)
+    let outputURL = try getOutputFolderURL().appendingPathComponent("Book2")
+    try FileManager.default.ensureDirectoryExists(at: outputURL)
+    try book.writeEpub(to: outputURL.appending(component: "Book2.epub"))
+}
 
 func book3() throws -> Book {
     Book(
@@ -122,7 +117,7 @@ func book3() throws -> Book {
         language: "ja",
         sections: [
             BookSection(
-                title: "始まり",
+                title: "<ruby>始<rt>はじ</rt>まり</ruby>",
                 content: [],
                 subsections: [
                     BookSection(
@@ -155,28 +150,27 @@ func book3() throws -> Book {
 func testBook3Navigation() throws {
     let builder = FancyVrtlEpubBuilder(
         depth: .two,
-        sectionTitleFormatter: { number, title in
-            if number.count == 1 {
-                return "#\(number[0]). \(title)"
-            } else {
-                return "##\(number[0]).\(number[1]). \(title)"
-            }
-        }
+        colophon: [
+            .heading("『タイトル』"),
+            .paragraph("作者"),
+            .horizontalRule,
+            .paragraph("EPUB by Swift package EpubBuilder"),
+        ]
     )
 
     let expectedNavigation = """
     <nav epub:type="toc">
     <h1>Contents</h1>
     <ol>
-    <li><a href="text/p-0001.xhtml">#1. 始まり</a>
+    <li><a href="p-001.xhtml"><ruby>始<rt>はじ</rt>まり</ruby></a>
     <ol>
-    <li><a href="text/p-0001.xhtml">##1.1. 第１話</a></li>
-    <li><a href="text/p-0002.xhtml">##1.2. 第２話</a></li>
+    <li><a href="p-001.xhtml">第１話</a></li>
+    <li><a href="p-002.xhtml">第２話</a></li>
     </ol>
     </li>
-    <li><a href="text/p-0003.xhtml">#2. 終わり</a>
+    <li><a href="p-003.xhtml">終わり</a>
     <ol>
-    <li><a href="text/p-0003.xhtml">##2.1. 第３話</a></li>
+    <li><a href="p-003.xhtml">第３話</a></li>
     </ol>
     </li>
     </ol>
@@ -184,4 +178,20 @@ func testBook3Navigation() throws {
     """
     let actualNavigation = try builder.buildNavigationBody(book: book3())
     #expect(expectedNavigation == actualNavigation)
+}
+
+@Test func testBook3Epub() throws {
+    let book = try book3().toEpub(builder: FancyVrtlEpubBuilder(
+        depth: .two,
+        colophon: [
+            .heading("『タイトル』"),
+            .paragraph("作者"),
+            .horizontalRule,
+            .paragraph("EPUB by Swift package EpubBuilder"),
+        ]
+    ), bookId: .zero)
+    let outputURL = try getOutputFolderURL().appendingPathComponent("Book3")
+    try FileManager.default.ensureDirectoryExists(at: outputURL)
+    try book.write(to: outputURL)
+    try book.writeEpub(to: outputURL.appending(component: "Book3.epub"))
 }
